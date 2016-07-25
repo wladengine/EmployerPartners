236bd6abee6e33547c3fe3277d628c9b81c6350b
wladengine <wladengine@yandex.ru>
2016-07-25 19:21:49 +0300

    1

--- EmployerPartners/EmpoyerPartners.Designer.cs
+++ EmployerPartners/EmpoyerPartners.Designer.cs
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
-﻿// T4 code generation is enabled for model 'D:\Projects\EmployerPartners\EmployerPartners\EmpoyerPartners.edmx'. 
-// To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
-// property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
-// is open in the designer.
-=======
-﻿// Создание кода T4 для модели "C:\Users\st900264\Documents\GitTest\EmployerPartners\EmpoyerPartners.edmx" включено. 
+// Создание кода T4 для модели "C:\Users\st900264\Documents\GitTest\EmployerPartners\EmpoyerPartners.edmx" включено. 
 // Чтобы включить формирование кода прежних версий, измените значение свойства "Стратегия создания кода" конструктора
 // на "Legacy ObjectContext". Это свойство доступно в окне "Свойства", если модель
 // открыта в конструкторе.
->>>>>>> origin/master
 
 // Если не сформированы контекст и классы сущности, возможная причина в том, что вы создали пустую модель, но
 // еще не выбрали версию Entity Framework для использования. Чтобы сформировать класс контекста и классы сущностей
